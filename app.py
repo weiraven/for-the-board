3f22d9652ca0327c55a92c7167ce3aab65dede8f
Raven Wei <weiwardraven@gmail.com>
2023-11-11 23:06:30 -0500

    adding db config and initialization to app.py

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for
+from models import db
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
+
+# DB connection
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    f'postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASS")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}'
+
+db.init_app(app)
 
 @app.route('/', methods=('GET', 'POST'))
 def index():  
