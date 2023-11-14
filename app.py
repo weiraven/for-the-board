a68c5b9a36db17963d7d95b8729001ca4296b146
brandonhach <specialcambo@gmail.com>
2023-11-14 17:29:49 -0500

    Fixed quotation on the app.config postgresql uri

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 # DB connection
 app.config[
     'SQLALCHEMY_DATABASE_URI'
-] = f'postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}'
+] = f'postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASS")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}'
 
 db.init_app(app)
 
