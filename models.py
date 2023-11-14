f35c7bb04a43f2f26553845591b09fa7470eb710
Chang <74262047+ChaChang-Byte@users.noreply.github.com>
2023-11-14 17:18:37 -0500

    some typo and tablename updated

--- models.py
+++ models.py
@@ -4,7 +4,8 @@ from datetime import datetime
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'User'
+    user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), nullable=False, unique=True)
     email = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(100), nullable=False)
