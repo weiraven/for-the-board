dfc56f7ef86c8a7b7f4b2d0270ac92298c24f639
Raven Wei <twei1@uncc.edu>
2023-11-14 21:13:20 -0500

    renamed User to Player and added ForumPost create table statement

--- ftb_schema.sql
+++ ftb_schema.sql
@@ -1,16 +1,29 @@
 -- Create users table
--- Keep the double quotes because otherwise PostgreSQL treats User as a reserved keyword
-CREATE TABLE "User" (
+-- Changing "User" to Player since User as a reserved keyword and the quotes are annoying
+CREATE TABLE Player (
     user_id SERIAL NOT NULL,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    email VARCHAR(100) NOT NULL UNIQUE,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(100) NOT NULL,
     date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (user_id)
 );
 
--- When interacting with the "User" table in DataGrip, always remember to use the quotes!
-INSERT INTO "User" (username, email, password)
+CREATE TABLE ForumPost (
+    post_id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT,
+    author_id INT,
+    author_name VARCHAR(255),
+    time_posted TIMESTAMP DEFAULT,
+    upvotes INT,
+    downvotes INT,
+    parent_post_id INT,
+    FOREIGN KEY (author_id) REFERENCES Player (user_id),
+    FOREIGN KEY (parent_post_id) REFERENCES ForumPost(post_id)
+);
+
+INSERT INTO Player (username, email, password)
 VALUES
     ('dnitsavo', 'dnitsavo@uncc.edu', 'password'),
     ('chachachang', 'pchang13@uncc.edu', 'password'),
