095bf3489bc4e46440d39744f1424cc7ce96945e
Chang <74262047+ChaChang-Byte@users.noreply.github.com>
2023-11-15 21:36:11 -0500

    New methods implemented for database changes

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from models import db
 from dotenv import load_dotenv
 from flask_socketio import SocketIO
-from repositories.src.user_repository import user_repository_singleton
+from repositories.src.user_repository import player_repository_singleton
 
 load_dotenv()
 app = Flask(__name__)
@@ -26,13 +26,9 @@ def index():
 def forum():
     return render_template('forum.html')
 
-@app.get('/signup')
-def signup():
-    return render_template('signup.html')
-
 @app.get('/chatsession')
 def chat():
-    users = user_repository_singleton.get_all_users()
+    users = player_repository_singleton.get_all_users()
     return render_template('chat_session.html', users=users)
 
 @socketio.on('set_active_user')
@@ -44,13 +40,13 @@ def set_active_user(user_id):
 @socketio.on('connected')
 def handle_connect(json):
     username = json.get('username')
-    user = user_repository_singleton.get_user_by_username(username)
+    user = player_repository_singleton.get_user_by_username(username)
     if user:
         set_active_user(user.user_id)
 
 @socketio.on('message')
 def handle_message(json):
-    user = user_repository_singleton.get_user_by_id(active_user_id)
+    user = player_repository_singleton.get_user_by_id(active_user_id)
     if user:
         socketio.emit('message', {'username': user.username, 'message': json['message']})
 
