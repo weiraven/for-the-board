7daa12b38c850e543d6c45d3b4682586884960df
Chang <74262047+ChaChang-Byte@users.noreply.github.com>
2023-11-15 21:36:44 -0500

    Methods for calling from database for chat session

--- repositories/src/user_repository.py
+++ repositories/src/user_repository.py
@@ -1,14 +1,14 @@
-from models import db, User
+from models import db, Player
 
-class UserRepository:
+class PlayerRepository:
 
     def get_all_users(self):
-            return User.query.all()
+            return Player.query.all()
         
-    def get_user_by_id(self, user_id: int) -> User | None:
-            return User.query.get(user_id)
+    def get_user_by_id(self, user_id: int) -> Player | None:
+            return Player.query.get(user_id)
     
-    def get_user_by_username(self, username: str) -> User | None:
-        return User.query.filter_by(username=username).first()
+    def get_user_by_username(self, username: str) -> Player | None:
+        return Player.query.filter_by(username=username).first()
         
-user_repository_singleton = UserRepository()
+player_repository_singleton = PlayerRepository()
