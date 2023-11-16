adcdf535d93381adae860aca758e01d51081f8a8
Raven Wei <weiwardraven@gmail.com>
2023-11-15 21:43:12 -0500

    added line 17 back in, necessary for chat features per PChang

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask_socketio import SocketIO
 load_dotenv()
 app = Flask(__name__)
 
+# DB Connection
 app.config[
     'SQLALCHEMY_DATABASE_URI'
 ] = f'postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASS")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}'
@@ -14,6 +15,8 @@ app.config[
 db.init_app(app)
 
 socketio = SocketIO(app)
+
+active_user_id = None
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
@@ -52,32 +55,32 @@ def create_post():
         return redirect(url_for('forum'))  # redirect back to GuildBoard main page
     return render_template('create_post.html')
 
-# @app.get('/chatsession')
-# def chat():
-#     return render_template('chat_session.html', users=users)
+@app.get('/chatsession')
+def chat():
+    return render_template('chat_session.html', users=users)
 
-# @socketio.on('set_active_user')
-# def set_active_user(user_id):
-#     global active_user_id
-#     active_user_id = user_id
-#     socketio.emit('active_user_changed', user_id)
+@socketio.on('set_active_user')
+def set_active_user(user_id):
+    global active_user_id
+    active_user_id = user_id
+    socketio.emit('active_user_changed', user_id)
 
-# @socketio.on('connected')
-# def handle_connect(json):
-#     username = json.get('username')
-#     user_id = None
-#     for uid, uname in users.items():
-#         if uname == username:
-#             user_id = uid
-#             break
-#     if user_id:
-#         set_active_user(user_id)
+@socketio.on('connected')
+def handle_connect(json):
+    username = json.get('username')
+    user_id = None
+    for uid, uname in users.items():
+        if uname == username:
+            user_id = uid
+            break
+    if user_id:
+        set_active_user(user_id)
 
-# @socketio.on('message')
-# def handle_message(json):
-#     username = users.get(active_user_id)  # Get the username based on active_user_id
-#     if username:
-#         socketio.emit('message', {'username': username, 'message': json['message']})
+@socketio.on('message')
+def handle_message(json):
+    username = users.get(active_user_id)  # Get the username based on active_user_id
+    if username:
+        socketio.emit('message', {'username': username, 'message': json['message']})
 
-# if __name__ == '__main__':
-#     socketio.run(app, debug=True)
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
