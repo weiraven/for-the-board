4147f20dfbab96c77670f4c351d5279239f9b667
Raven Wei <weiwardraven@gmail.com>
2023-11-28 00:12:04 -0500

    combined duplicate user_repository.py files contents with this file to remove redundant files

--- repositories/src/user_repository.py
+++ repositories/src/user_repository.py
@@ -1,14 +1,27 @@
-from models import db, Player
+from models import db, User
 
-class PlayerRepository:
+class UserRepository:
 
     def get_all_users(self):
-            return Player.query.all()
+            return User.query.all()
         
-    def get_user_by_id(self, user_id: int) -> Player | None:
-            return Player.query.get(user_id)
+    def get_user_by_id(self, user_id: int) -> User | None:
+            return User.query.get(user_id)
     
-    def get_user_by_username(self, username: str) -> Player | None:
-        return Player.query.filter_by(username=username).first()
-        
-player_repository_singleton = PlayerRepository()
+    def get_user_by_username(self, username: str) -> User | None:
+        return User.query.filter_by(username=username).first()
+
+    def get_user(self, user_id):
+        one_user = User.query.get(user_id)
+        return one_user
+    
+    def create_user(self, user_id, username, email, password, date_created):
+        new_user = User(user_id=user_id, username=username, email=email, password=password, date_created=date_created)
+        db.session.add(new_user)
+        db.session.commit()
+        return new_user
+    
+    def search_user(self, username):
+        return User.query.filter(User.username.ilike(f'%{username}%')).all()
+    
+player_repository_singleton = UserRepository()
