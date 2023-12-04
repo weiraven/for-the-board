08cf07e104ccb7d09bcaa898e0948785fac6841e
Raven Wei <weiwardraven@gmail.com>
2023-12-04 01:28:55 -0500

    edited sample data entry format to conform with new web app features

--- ftb_schema.sql
+++ ftb_schema.sql
@@ -17,13 +17,12 @@ CREATE TABLE ForumPost (
     content TEXT,
     author_id INT,
     time_posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    upvotes INT, -- removed downvotes since downvote button should just decrement upvotes
+    upvotes INT,
     parent_post_id INT,
-    flairs VARCHAR(255), -- added flairs directly to CREATE TABLE statement
-    category VARCHAR(255); -- added category 12/03/2023
+    flairs VARCHAR(255),  
+    category VARCHAR(255);  
     FOREIGN KEY (author_id) REFERENCES Player (user_id),
-    FOREIGN KEY (parent_post_id) REFERENCES ForumPost(post_id)
-    
+    FOREIGN KEY (parent_post_id) REFERENCES ForumPost (post_id)
 );
 
 -- Now that we have sign-up and login auth implemented. We can no longer manually edit
@@ -41,14 +40,13 @@ CREATE TABLE ForumPost (
 
 -- You will need to have a user account with user_id 1 and 4 at least for the following forum posts to work.
 
-INSERT INTO ForumPost (title, content, author_id)
+INSERT INTO ForumPost (title, content, author_id, upvotes, flairs, category)
 VALUES
-    ('TEST POST! TEST POST! TEST POST!', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos aspernatur omnis, perferendis fuga quaerat ex, id sed dolores minus dolorum veritatis dolor commodi voluptates, praesentium beatae error consectetur hic. Fuga consequuntur illo odit sit eveniet laudantium? Impedit laboriosam mollitia ut iusto harum voluptates, nostrum nam esse tempora facilis cupiditate.', 1),
-    ('[LFG][sHC][static][Aether][DC Travel][SGE] Mit Healer LFG 7.0+', '<p>I''m looking for a week 1-2 clearing static for 7.0 savage and ultimate content. I strongly prefer to play Sage but I can swap to Scholar for the right group. My long-time static and friend-group is disbanding after this tier. I''ve cleared all past savage raid tiers and ultimates while they were relevant (before potency/level-cap increase soft nerfs) except UCOB and TOP.</p><p>- My logs: https://www.fflogs.com/character/na/cactuar/spiral%20sun<br>- My VODs: https://www.twitch.tv/weiward/videos<br></p><p>Availability: I''m currently back in school full-time, so unfortunately I won''t have PTO/vacation time to alarm-clock week 1. But I will be openly available at the following times:</p><p>- Mon/Wed/Fri: After 6 PM EST<br>- Tues/Thur - After 10 PM EST<br>- Saturday & Sunday: All Day<br></p><p>Please send me a DM via Discord if interested, cheers!</p>', 1),
-    ('LF3M for fresh D&D 5e campaign', '<p>Calling All Adventurers!</p><p>Are you ready to embark on an epic journey? Do you have the courage to face the unknown, the wisdom to solve intricate puzzles, and the charisma to lead or negotiate your way out of tricky situations? If so, we want you to join our virtual Dungeons & Dragons 5e campaign!</p><p>Our campaign is set in a richly detailed world, teeming with diverse cultures, ancient mysteries, and untold dangers. Whether you''re a seasoned veteran or a newcomer to the game, you''ll find a place at our table!</p>', 3),
-    ('How to pick up elves in a dungeon', 'Step 1: Just roll a nat 20 on your rizz check 4head lol', 4)
+    ('TEST POST! TEST POST! TEST POST!', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos aspernatur omnis, perferendis fuga quaerat ex, id sed dolores minus dolorum veritatis dolor commodi voluptates, praesentium beatae error consectetur hic. Fuga consequuntur illo odit sit eveniet laudantium? Impedit laboriosam mollitia ut iusto harum voluptates, nostrum nam esse tempora facilis cupiditate.', 1, 0, 'Strategy, Miscellaneous', 'community-square'),
+    ('[LFG][sHC][static][Aether][DC Travel][SGE] Mit Healer LFG 7.0+', '<p>I''m looking for a week 1-2 clearing static for 7.0 savage and ultimate content. I strongly prefer to play Sage but I can swap to Scholar for the right group. My long-time static and friend-group is disbanding after this tier. I''ve cleared all past savage raid tiers and ultimates while they were relevant (before potency/level-cap increase soft nerfs) except UCOB and TOP.</p><p>- My logs: https://www.fflogs.com/character/na/cactuar/spiral%20sun<br>- My VODs: https://www.twitch.tv/weiward/videos<br></p><p>Availability: I''m currently back in school full-time, so unfortunately I won''t have PTO/vacation time to alarm-clock week 1. But I will be openly available at the following times:</p><p>- Mon/Wed/Fri: After 6 PM EST<br>- Tues/Thur - After 10 PM EST<br>- Saturday & Sunday: All Day<br></p><p>Please send me a DM via Discord if interested, cheers!</p>', 1, 0, 'FFXIV, LFG, Raid', 'community-square'),
+    ('LF3M for fresh D&D 5e campaign', '<p>Calling All Adventurers!</p><p>Are you ready to embark on an epic journey? Do you have the courage to face the unknown, the wisdom to solve intricate puzzles, and the charisma to lead or negotiate your way out of tricky situations? If so, we want you to join our virtual Dungeons & Dragons 5e campaign!</p><p>Our campaign is set in a richly detailed world, teeming with diverse cultures, ancient mysteries, and untold dangers. Whether you''re a seasoned veteran or a newcomer to the game, you''ll find a place at our table!</p>', 3, 0, 'D&D, LFM', 'community-square'),
+    ('How to pick up elves in a dungeon', 'Step 1: Just roll a nat 20 on your rizz check 4head lol', 4, 0, 'Fantasy, Humor', 'community-square')
 ;
-
 
 --Datasets you can use for Player & ForumPost table - brandon
 
