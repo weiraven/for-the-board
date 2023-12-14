2120b41548b45a3183772c4807acd54e546f3004
Rachel <rachele912@gmail.com>
2023-12-14 17:39:09 -0500

    fix spacing issue

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ load_dotenv()
 app = Flask(__name__)
 
 # For local DB connection only:
-#app.config[
+# app.config[
 #     'SQLALCHEMY_DATABASE_URI'
 # ] = f'postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASS")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}'
 
