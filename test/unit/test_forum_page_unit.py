97bb4bf3210937490fe23e441e73cc56e83354a3
brandonhach <specialcambo@gmail.com>
2023-12-13 20:19:50 -0500

    Added more unit test and e2e test

--- test/unit/test_forum_page_unit.py
+++ test/unit/test_forum_page_unit.py
@@ -1,6 +1,6 @@
 import pytest
 from flask.testing import FlaskClient
-from app import app, db
+from app import app
 
 @pytest.fixture()
 def test_app() -> FlaskClient:
@@ -22,9 +22,9 @@ def test_forum_topics_links(test_app: FlaskClient) -> None:
     response = test_app.get('/forum/bug-report-technical')
     assert response.status_code == 200
     
-def test_create_post_route(test_app: FlaskClient) -> None:
+def test_create_post_route_unauthorized_access(test_app: FlaskClient) -> None:
     response = test_app.get('/create_post')
-    assert response.status_code == 200
+    assert response.status_code == 302
     
 def test_forum_search_route(test_app: FlaskClient) -> None:
     response = test_app.get('/forum/search/')
