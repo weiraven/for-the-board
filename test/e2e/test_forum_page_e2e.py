1020acdd6a6e06ca90c7b7b210229c95521cad39
brandonhach <specialcambo@gmail.com>
2023-12-14 13:53:40 -0500

    Static picture art for custom error page

--- test/e2e/test_forum_page_e2e.py
+++ test/e2e/test_forum_page_e2e.py
@@ -15,3 +15,8 @@ def test_forum_topics(test_app: FlaskClient) -> None:
     assert b'<a href="/forum/gamemaster-corner"' in response.data
     assert b'<a href="/forum/bug-report-technical"' in response.data
     assert b'<a href="/forum"' in response.data
+    
+def test_check_forum_description_exist(test_app: FlaskClient) -> None:
+    response = test_app.get('/forum')
+    assert response.status_code == 200
+    assert b'<p class="card-text">All Topics - GuildBoard Main: Your one-stop destination for general discussions, announcements, and everything else not covered in other categories.</p>'
